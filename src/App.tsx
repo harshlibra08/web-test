1ccae608418a0fd0d76d565126f3428a243086de
Harsh Choudhary <harsh.c@libratherm.com>
2023-07-31 17:37:38 +0530

    card

--- src/App.tsx
+++ src/App.tsx
@@ -400,8 +400,84 @@ function App() {
 				data={['India', 'USA', 'Iran', 'France', 'Canada', 'Antartica']}
 				style={{width:"288px", height: "43px", fontWeight:"400", color:"#555459", fontSize:"16px", margin:" 51px auto", }}
 			/>
+				<Paper>
+				<Group position="center">
+				<Card
+					shadow="sm"
+					padding="xl"
+					component="a"
+					
+					target="_blank"
+					>
+					<Card.Section>
+						<Image
+						src="https://images.unsplash.com/photo-1579227114347-15d08fc37cae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
+						height={391}
+						width = {391}
+						alt="No way!"
+						/>
+					</Card.Section>
 
-			
+					<Text weight={500} size="xs" mt="xs">
+						You&apos;ve won a million dollars in cash!
+					</Text>
+
+					<Text mt="xs" color="dimmed" size="xs">
+						Please click anywhere on this card to claim your reward, this is not a fraud, trust us
+					</Text>
+					</Card>
+
+					<Card
+					shadow="sm"
+					padding="xl"
+					component="a"
+					
+					target="_blank"
+					>
+					<Card.Section>
+						<Image
+						src="https://images.unsplash.com/photo-1579227114347-15d08fc37cae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
+						height={391}
+						width = {391}
+						alt="No way!"
+						/>
+					</Card.Section>
+
+					<Text weight={500} size="xs" mt="xs">
+						You&apos;ve won a million dollars in cash!
+					</Text>
+
+					<Text mt="xs" color="dimmed" size="xs">
+						Please 
+					</Text>
+					</Card>
+
+					<Card
+					shadow="sm"
+					padding="xl"
+					component="a"
+					
+					target="_blank"
+					>
+					<Card.Section>
+						<Image
+						src="https://images.unsplash.com/photo-1579227114347-15d08fc37cae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
+						height={391}
+						width = {391}
+						alt="No way!"
+						/>
+					</Card.Section>
+
+					<Text weight={500} size="xs" mt="xs">
+						You&apos;ve won a million dollars in cash!
+					</Text>
+
+					<Text mt="xs" color="dimmed" size="xs">
+						Please click anywhere on this card to claim your reward, this is not a fraud, trust us
+					</Text>
+					</Card>
+					</Group>
+					</Paper>
 				
 		</MantineProvider>
 		
